5ac8b7b47fdf9afc1e8ed7f2c7070ed48b8985cd
lvyufeng <lvyufeng@cqu.edu.cn>
2022-07-09 20:15:01 +0800

    add acgan and upsample

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Collection of MindSpore implementations of Generative Adversarial Network variet
   * [Installation](#installation)
   * [Datasets](#datasets)
   * [Implementations](#implementations)
-    <!-- + [Auxiliary Classifier GAN](#auxiliary-classifier-gan) -->
+    + [Auxiliary Classifier GAN](#auxiliary-classifier-gan)
     + [Adversarial Autoencoder](#adversarial-autoencoder)
     <!-- + [BEGAN](#began)
     + [BicycleGAN](#bicyclegan)
@@ -58,6 +58,28 @@ $ bash scripts/download_mnist.sh
 ```
 
 ## Implementations
+
+### Auxiliary Classifier GAN
+_Auxiliary Classifier Generative Adversarial Network_
+
+#### Authors
+Augustus Odena, Christopher Olah, Jonathon Shlens
+
+#### Abstract
+Synthesizing high resolution photorealistic images has been a long-standing challenge in machine learning. In this paper we introduce new methods for the improved training of generative adversarial networks (GANs) for image synthesis. We construct a variant of GANs employing label conditioning that results in 128x128 resolution image samples exhibiting global coherence. We expand on previous work for image quality assessment to provide two new analyses for assessing the discriminability and diversity of samples from class-conditional image synthesis models. These analyses demonstrate that high resolution samples provide class information not present in low resolution samples. Across 1000 ImageNet classes, 128x128 samples are more than twice as discriminable as artificially resized 32x32 samples. In addition, 84.7% of the classes have samples exhibiting diversity comparable to real ImageNet data.
+
+[[Paper]](https://arxiv.org/abs/1610.09585) [[Code]](src/acgan/acgan.py)
+
+#### Run Example
+```
+$ cd src/acgan/
+$ python3 acgan.py
+```
+
+<p align="center">
+    <img src="assets/acgan.gif" width="360"\>
+</p>
+
 
 ### Adversarial Autoencoder
 _Adversarial Autoencoder_
--- src/layers.py
+++ src/layers.py
@@ -1,6 +1,31 @@
 import math
+import mindspore
 import mindspore.nn as nn
-from mindspore.common.initializer import initializer, HeUniform, Uniform, _calculate_fan_in_and_fan_out
+import mindspore.ops as ops
+from mindspore import Tensor
+from mindspore.common.initializer import initializer, HeUniform, Uniform, Normal, _calculate_fan_in_and_fan_out
+from mindspore.ops.operations.image_ops import ResizeBilinearV2, ResizeLinear1D
+from mindspore.ops._primitive_cache import _get_cache_prim
+from mindspore.ops import constexpr
+
+class Conv2d(nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, pad_mode='same', padding=0, dilation=1, group=1, has_bias=True):
+        super().__init__(in_channels, out_channels, kernel_size, stride, pad_mode, padding, dilation, group, has_bias, weight_init='normal', bias_init='zeros')
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+        self.weight.set_data(initializer(HeUniform(math.sqrt(5)), self.weight.shape))
+        #self.weight = Parameter(initializer(HeUniform(math.sqrt(5)), self.weight.shape), name='weight')
+        if self.has_bias:
+            fan_in, _ = _calculate_fan_in_and_fan_out(self.weight.shape)
+            bound = 1 / math.sqrt(fan_in)
+            self.bias.set_data(initializer(Uniform(bound), [self.out_channels]))
+
+class Embedding(nn.Embedding):
+    def __init__(self, vocab_size, embedding_size, use_one_hot=False, embedding_table='normal', dtype=mindspore.float32, padding_idx=None):
+        if embedding_table == 'normal':
+            embedding_table = Normal(1.0)
+        super().__init__(vocab_size, embedding_size, use_one_hot, embedding_table, dtype, padding_idx)
 
 class Dense(nn.Dense):
     def __init__(self, in_channels, out_channels, has_bias=True, activation=None):
@@ -13,3 +38,58 @@ class Dense(nn.Dense):
             fan_in, _ = _calculate_fan_in_and_fan_out(self.weight.shape)
             bound = 1 / math.sqrt(fan_in)
             self.bias.set_data(initializer(Uniform(bound), [self.out_channels]))
+
+@constexpr
+def _check_scale_factor(shape, scale_factor):
+    if isinstance(scale_factor, tuple) and len(scale_factor) != len(shape[2:]):
+        raise ValueError(f"the number of 'scale_fator' must match to inputs.shape[2:], "
+                         f"but get scale_factor={scale_factor}, inputs.shape[2:]={shape[2:]}")
+
+def _interpolate_output_shape(shape, scales, sizes, mode):
+    """calculate output shape"""
+    if sizes is not None:
+        if mode == "nearest":
+            return sizes
+        return Tensor(sizes)
+
+    ret = ()        
+    for i in range(len(shape[2:])):
+        if isinstance(scales, float):
+            out_i = int(scales * shape[i+2])
+        else:
+            out_i = int(scales[i] * shape[i+2])
+        ret = ret + (out_i,)
+    if mode == "nearest":
+        return ret
+    return Tensor(ret)
+
+class Upsample(nn.Cell):
+    def __init__(self, size = None, scale_factor = None,
+                 mode: str = 'nearest', align_corners = False):
+        super().__init__()
+        if mode not in ['nearest', 'linear', 'bilinear']:
+            raise ValueError(f'do not support mode :{mode}.')
+        if size and scale_factor:
+            raise ValueError(f"can not set 'size' and 'scale_fator' at the same time.")
+        self.size = size
+        if isinstance(scale_factor, tuple):
+            self.scale_factor = tuple(float(factor) for factor in scale_factor)
+        else:
+            self.scale_factor = float(scale_factor) if scale_factor else None
+        self.mode = mode
+        self.align_corners = align_corners
+
+    def construct(self, inputs):
+        inputs_shape = inputs.shape
+        _check_scale_factor(inputs_shape, self.scale_factor)
+        sizes = _interpolate_output_shape(inputs_shape, self.scale_factor, self.size, self.mode)
+        if self.mode == 'nearest':
+            interpolate = _get_cache_prim(ops.ResizeNearestNeighbor)(sizes, self.align_corners)
+            return interpolate(inputs)
+        elif self.mode == 'linear':
+            interpolate = _get_cache_prim(ResizeLinear1D)('align_corners' if self.align_corners else 'half_pixel')
+            return interpolate(inputs, sizes)
+        elif self.mode == 'bilinear':
+            interpolate = _get_cache_prim(ResizeBilinearV2)(self.align_corners, True if self.align_corners==False else False)
+            return interpolate(inputs, sizes)
+        return inputs
